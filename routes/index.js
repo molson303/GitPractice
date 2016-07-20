47d55c7518c9831af0774754dbb40d8eb436849f
Jonathan Huxhold <jonathanhuxhold@Jonathans-MacBook-Pro.local>
2016-07-20 10:19:32 -0600

    added dog route

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/dog', function (req, res, next) {
+  res.send('woof woof, I am a dog')
+})
+
+module.exports = router;
